4a56e5ee8ad322f4d2e7072911d9b35ec4792ddd
Prince <elavarasa.003@gmail.com>
2024-07-29 13:56:25 +0530

    Add ButtonC component with customizable properties
    This commit adds a new styled button component called ButtonC. The component allows customization of properties such as borderRadius, width, and height. It also includes the necessary import paths and stories for the component.

--- src/index.js
+++ src/index.js
@@ -2,3 +2,4 @@
 // Exporting components
 export { default as ButtonA } from './components/Buttons/ButtonAA/ButtonAA';
 export { default as ButtonB } from './components/Buttons/ButtonAB/ButtonAB';
+export { default as ButtonC } from './components/Buttons/ButtonOnChangeColor/ButtonOnChangeColor';

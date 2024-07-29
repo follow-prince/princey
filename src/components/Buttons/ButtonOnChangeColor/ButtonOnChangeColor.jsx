4c025e8740e5150397603d93637b1ef216514128
Prince <elavarasa.003@gmail.com>
2024-07-29 13:15:34 +0530

    Update ButtonOnChangeColor component
    This commit updates the ButtonOnChangeColor component by adding a new styled button with customizable properties such as borderRadius, width, and height. It also includes the necessary import paths and stories for the component.

---
+++ src/components/Buttons/ButtonOnChangeColor/ButtonOnChangeColor.jsx
@@ -0,0 +1,56 @@
+import React from "react";
+import styled from "styled-components";
+
+const Button = styled.button`
+  display: inline-block;
+  padding: 10px 20px;
+  font-size: 24px;
+  font-weight: bold;
+  text-align: center;
+  text-decoration: none;
+  color: #fff;
+  background-color: #ff5252;
+  border: 2px solid #000;
+  border-radius: ${(props) => props.borderRadius || "10px"};
+  box-shadow: 5px 5px 0px #000;
+  transition: all 0.3s ease;
+  cursor: pointer;
+  width: ${(props) => props.width || "auto"};
+  height: ${(props) => props.height || "auto"};
+
+  &:hover {
+    background-color: #fff;
+    color: #ff5252;
+    border: 2px solid #ff5252;
+    box-shadow: 5px 5px 0px #ff5252;
+  }
+
+  &:active {
+    background-color: #fcf414;
+    box-shadow: none;
+    transform: translateY(4px);
+  }
+`;
+
+const ButtonOnChangeColor = ({
+  type,
+  borderRadius,
+  width,
+  height,
+  children,
+  onClick,
+  ...props
+}) => (
+  <Button
+    type={type}
+    borderRadius={borderRadius}
+    width={width}
+    height={height}
+    onClick={onClick}
+    {...props}
+  >
+    {children}
+  </Button>
+);
+
+export default ButtonOnChangeColor;
